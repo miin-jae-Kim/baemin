2ae22efca394fb900b3236113fecbb650d9ab548
miin-jae-Kim <bapbodanbbang@gmail.com>
2022-10-05 11:40:36 +0900

    Add: Entity, User

--- src/main/java/com/homes/baemin/domain/User.java
+++ src/main/java/com/homes/baemin/domain/User.java
@@ -1,13 +1,26 @@
 package com.homes.baemin.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
 
 @Entity
+@Table(
+        uniqueConstraints = {
+            @UniqueConstraint(name = "PHONE_UNIQUE", columnNames = {"phone"})
+        })
 public class User {
+
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(length = 64)
+    @NotNull
+    private String name;
+
+    @Column
+    @NotNull
+    private int phone;
 }
